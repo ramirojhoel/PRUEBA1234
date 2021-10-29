3d78d1e6ddd5da53d127380726de82e72d0c4e35
ramirojhoel <ramirojhoel1998@gmail.com>
2021-10-29 00:08:52 -0400

    puebadehoyversion

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EJEMPLOPRUEBAHOY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("como sea !");
+
+
+        }
+
+        
+    }
+}
